5d328c007ded448f22e459a17d40708424b68e8d
Denis <denb6377@gmail.com>
2022-05-20 19:12:31 +0300

    add testsqrt, ed SQRServiceTest

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,3 +1,19 @@
 package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
 public class SQRServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testsqrt.csv")
+    public void test(int a, int b, int expected) {
+        SQRService service = new SQRService();
+        int actual = service.sqrtCeil(a, b);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
