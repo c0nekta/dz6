e207270c233cf2d315b6858ba6e740fcda7979d0
Denis <denb6377@gmail.com>
2022-05-20 18:28:43 +0300

    add if >=a SQRService

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,7 +3,8 @@ package ru.netology.sqr;
 public class SQRService {
     public int sqrtCeil(int a, int b) {
         int amount = 0;
-        for (int i = a; i <= b; i++) {
+        for (int i = 0; i <= b; i++) {
+            if (i * i >= a)
             if (i * i <= b) {
                 amount++;
             }
